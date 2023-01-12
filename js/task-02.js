6089c091a6c860e4b126b97164e46bf86c2848ab
Olga <111655363+HronaOlha@users.noreply.github.com>
2023-01-12 15:32:39 +0200

    Зроблена задача 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,26 @@
+// Напиши скрипт, який для кожного елемента масиву ingredients:
+
+// 1. Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+// 2. Додасть назву інгредієнта як його текстовий вміст.
+// 3. Додасть елементу клас item.
+// 4. Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+ingredients.map((ingredient) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = ingredient;
+  listItem.classList.add("item");
+  list.append(listItem);
+});
+
+console.log(list);
