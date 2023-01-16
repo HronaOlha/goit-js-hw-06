c2125d5f9e70bd3e4b2d46b05c3d9ae4ce2e8737
Olga <111655363+HronaOlha@users.noreply.github.com>
2023-01-16 23:26:01 +0200

    Відредаговано задачу 8

--- js/task-08.js
+++ js/task-08.js
@@ -21,6 +21,6 @@ function handleSubmit(event) {
     alert("Please fill in all the fields!");
   }
 
-  console.log(`Login: ${email.value}, Password: ${password.value}`);
+  console.log({ email: email.value, password: password.value });
   event.currentTarget.reset();
 }
